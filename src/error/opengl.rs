a6e802b70518d0a01c1f347749e950d5e3738d71
Luc Drenth <luc@tinypixels.nl>
2023-05-14 20:24:31 +0200

    add backtrace for opengl errors

--- src/error/opengl.rs
+++ src/error/opengl.rs
@@ -1,9 +1,20 @@
+use std::backtrace::Backtrace;
+
 pub fn gl_check_errors() {
     unsafe {
         let mut err = gl::GetError();
 
         while err != gl::NO_ERROR {
-            println!("gl error: {}", err);
+            let trace = Backtrace::capture();
+            match trace.status() {
+                std::backtrace::BacktraceStatus::Captured => {
+                    println!("gl error: {}\n{}", err, trace);
+                },
+                _ => {
+                    println!("gl error: {}", err);
+                }
+            }
+
             err = gl::GetError();
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::env;
+
 mod graphics;
 mod error;
 
 fn main() {
+    // TODO since backtrace can be slow in production, we need to disable this in release mode
+    env::set_var("RUST_BACKTRACE", "1");
+
     graphics::window::run(String::from("Lazuli"));
 }
