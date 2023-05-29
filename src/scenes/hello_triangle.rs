38ef5d17ecf7db79c94aab52a50e012744d1e592
Luc Drenth <luc@tinypixels.nl>
2023-05-29 20:57:10 +0200

    add example scene: hello_triangle

---
+++ src/scenes/hello_triangle.rs
@@ -0,0 +1,30 @@
+use crate::{graphics::{scene::Scene, material::Material, Triangle, mesh_renderer, shader::{ShaderProgram, PATH_MOVING_TRIANGLE_VERT, PATH_MOVING_TRIANGLE_FRAG}}};
+
+pub struct HelloTriangle {
+    material: Material,
+    triangle: Triangle,
+}
+
+impl HelloTriangle {
+    pub fn new() -> Result<Self, String> {
+        let program = ShaderProgram::new(PATH_MOVING_TRIANGLE_VERT, PATH_MOVING_TRIANGLE_FRAG).unwrap();
+        let material = Material::new(program);
+
+        let triangle = Triangle::new(&material.shader_program);
+
+        let result = Self { 
+            material,
+            triangle,
+        };
+
+        Ok(result)
+    }
+}
+
+impl Scene for HelloTriangle {
+    fn update(&mut self) {}
+
+    unsafe fn draw(&self) {
+        mesh_renderer::draw_triangle(&self.triangle, &self.material);
+    }
+}
--- src/scenes/mod.rs
+++ src/scenes/mod.rs
@@ -1,5 +1,7 @@
-mod texture_mask;
+mod hello_triangle;
 mod moving_triangle;
+mod texture_mask;
 
-pub use texture_mask::TextureMask;
+pub use hello_triangle::HelloTriangle;
 pub use moving_triangle::MovingTriangle;
+pub use texture_mask::TextureMask;
