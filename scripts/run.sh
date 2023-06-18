7083a044dcc3ba73cc168bf849ce73d7cd60c4f4
Luc Drenth <luc.drenth@hotmail.com>
2023-06-18 23:06:44 +0200

    improve run script comments

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Step in to parent directory
 cd "$(dirname "$0")/.."
 
-# Prevents recompiling dependencies on every run
+# Prevent recompiling dependencies on every run
 export CARGO_TARGET_DIR=../target/
 
+# Run program
 RUSTFLAGS="$RUSTFLAGS -A dead_code" cargo run
 
 exit 0
