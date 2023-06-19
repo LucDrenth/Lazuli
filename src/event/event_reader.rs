9f7ab72d27eb4b6b9e920d0d5ad4a0166db3cae4
Luc Drenth <luc.drenth@hotmail.com>
2023-06-19 13:28:52 +0200

    add close function to EventReader

--- src/event/event_reader.rs
+++ src/event/event_reader.rs
@@ -1,29 +1,40 @@
 use std::sync::mpsc::TryRecvError;
 
 pub struct EventReader<T: Clone + Sync> {
-    bus_reader: bus::BusReader<T>,
+    bus_reader: Option<bus::BusReader<T>>,
 }
 
 impl<T: Clone + Sync> EventReader<T> {
     pub fn new(bus_reader: bus::BusReader<T>) -> Self {
-        Self { bus_reader }
+        Self { bus_reader: Some(bus_reader) }
     }
 
     pub fn read(&mut self) -> Vec<T> {
         let mut result = Vec::<T>::new();
 
         loop {
-            match self.bus_reader.try_recv() {
-                Ok(value) => {
-                    result.push(value);
+            match &mut self.bus_reader {
+                Some(some) => {
+                    match some.try_recv() {
+                        Ok(value) => {
+                            result.push(value);
+                        }
+                        Err(TryRecvError::Empty) => {
+                            break;
+                        }
+                        Err(TryRecvError::Disconnected) => {} // the sender has hungup, maybe the event system got destroyed?
+                    }
                 }
-                Err(TryRecvError::Empty) => {
+                None => {
                     break;
                 }
-                Err(TryRecvError::Disconnected) => {} // the sender has hungup, maybe the event system got destroyed?
             }
         }
 
         return result;
+    }
+
+    pub fn close(&mut self) {
+        self.bus_reader.take();
     }
 }
--- src/scenes/hello_event_system.rs
+++ src/scenes/hello_event_system.rs
@@ -1,30 +1,37 @@
 use crate::{graphics::scene::Scene, lz_core_info, event::{WindowResizeEvent, EventSystem, EventReader}};
 
 pub struct HelloEventSystem {
-    window_resize_listener: EventReader<WindowResizeEvent>,
+    window_resize_listener1: EventReader<WindowResizeEvent>,
+    window_resize_listener2: EventReader<WindowResizeEvent>,
     nr_updates: u32,
 }
 
 impl Scene for HelloEventSystem {
     fn new(event_system: &mut EventSystem) -> Result<Self, String> {
-        let listener = event_system.register::<WindowResizeEvent>();        
+        let listener1 = event_system.register::<WindowResizeEvent>();
+        let listener2 = event_system.register::<WindowResizeEvent>();
         
         Ok(Self{
-            window_resize_listener: listener,
+            window_resize_listener1: listener1,
+            window_resize_listener2: listener2,
             nr_updates: 0,
         })
     }
 
     fn update(&mut self, event_system: &mut EventSystem) {
-        for event in self.window_resize_listener.read().iter() {
-            lz_core_info!("Window resize event: {} / {}", event.width, event.height);
+        for event in self.window_resize_listener1.read().iter() {
+            lz_core_info!("(1) Window resize event: {} / {}", event.width, event.height);
+        }
+
+        for event in self.window_resize_listener2.read().iter() {
+            lz_core_info!("(2) Window resize event: {} / {}", event.width, event.height);
         }
 
         self.nr_updates += 1;
 
         if self.nr_updates == 300 { // 5 seconds
-            lz_core_info!("removing WindowResizeEvent listener");
-            // event_system.remove_listener::<WindowResizeEvent>(&self.window_resize_listener_handle);
+            lz_core_info!("removing WindowResizeEvent listener 1");
+            self.window_resize_listener1.close();
         }
     }
 
