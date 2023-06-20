3b50ab687f69b22f55896086b40432ef56dfc3bc
Luc Drenth <luc.drenth@hotmail.com>
2023-06-20 19:52:12 +0200

    remove example scene 'HelloTranslate'

--- src/scenes/mod.rs
+++ src/scenes/mod.rs
@@ -2,7 +2,6 @@ mod hello_triangle;
 mod moving_triangle;
 mod texture_mask;
 mod hello_texture;
-mod hello_translate;
 mod hello_event_system;
 mod coordinate_system;
 
@@ -10,6 +9,5 @@ pub use hello_triangle::HelloTriangle;
 pub use moving_triangle::MovingTriangle;
 pub use texture_mask::TextureMask;
 pub use hello_texture::HelloTexture;
-pub use hello_translate::HelloTranslate;
 pub use hello_event_system::HelloEventSystem;
 pub use coordinate_system::CoordinateSystem;
