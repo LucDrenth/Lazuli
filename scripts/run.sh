7a6c0b74f4f1feb21d5f301388e180d7915a343d
Luc Drenth <luc.drenth@hotmail.com>
2023-06-25 21:23:38 +0200

    add vsc settings that prevent recompiling all libraries on every run

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,9 +3,6 @@
 # Step in to parent directory
 cd "$(dirname "$0")/.."
 
-# Prevent recompiling dependencies on every run
-export CARGO_TARGET_DIR=./target/
-
 # Run program
 RUSTFLAGS="$RUSTFLAGS -A dead_code" cargo run
 
