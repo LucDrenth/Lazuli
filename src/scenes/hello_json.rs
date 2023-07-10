f8608232d8edec67cb653844716a0577e35bd0cf
Luc Drenth <luc.drenth@hotmail.com>
2023-07-10 17:17:51 +0200

    add example scene for json serialization

---
+++ src/scenes/hello_json.rs
@@ -0,0 +1,35 @@
+use std::{fs, path::Path};
+
+use glam::Vec2;
+
+use serde::{Deserialize, Serialize};
+use crate::{graphics::scene::Scene, event::EventSystem, input::Input, lz_core_info};
+
+pub struct HelloJson {}
+impl Scene for HelloJson {
+    fn new(_event_system: &mut EventSystem, _window_size: Vec2) -> Result<Self, String> {
+        
+        let data = MyStruct {
+            id: 15,
+            name: "test Luc".to_string(),
+        };
+        let json = serde_json::to_string(&data).unwrap();
+        fs::write(Path::new("./data.json"), json).unwrap();
+
+        let json_to_read = fs::read_to_string(Path::new("./data.json")).unwrap();
+        let my_struct: MyStruct = serde_json::from_str(&json_to_read).unwrap();
+        lz_core_info!("{}: {}", my_struct.id, my_struct.name);
+
+        let result = Self { };
+        Ok(result)
+    }
+
+    fn update(&mut self, _: &mut EventSystem, _: &Input) {}
+    unsafe fn draw(&self) {}
+}
+
+#[derive(Serialize, Deserialize)]
+struct MyStruct {
+    pub id: u8,
+    pub name: String,
+}
--- src/scenes/mod.rs
+++ src/scenes/mod.rs
@@ -5,6 +5,7 @@ mod hello_texture;
 mod hello_event_system;
 mod coordinate_system;
 mod hello_text;
+mod hello_json;
 
 pub use hello_triangle::HelloTriangle;
 pub use moving_triangle::MovingTriangle;
@@ -13,3 +14,4 @@ pub use hello_texture::HelloTexture;
 pub use hello_event_system::HelloEventSystem;
 pub use coordinate_system::CoordinateSystem;
 pub use hello_text::HelloText;
+pub use hello_json::HelloJson;
