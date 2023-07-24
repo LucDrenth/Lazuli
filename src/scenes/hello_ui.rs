b0d4e3392e9871bdfba7e8443d0616e4841c987a
Luc Drenth <luc.drenth@hotmail.com>
2023-07-24 21:21:42 +0200

    add ui element positioning options to offset from the corners of the screens

--- src/scenes/hello_ui.rs
+++ src/scenes/hello_ui.rs
@@ -14,7 +14,7 @@ impl Scene for HelloUi {
         let mut interface: ui::Interface = ui::Interface::new(event_system, window_size);
 
         let button = Button::new("Click me!".to_string(), ButtonBuilder::new()
-            .with_position(ui::Position::FixedLeft(25.0))
+            .with_position(ui::Position::FixedBottomRight(25.0, 10.0))
             .with_padding(16.0)
         , &mut interface, asset_registry)?;
 
