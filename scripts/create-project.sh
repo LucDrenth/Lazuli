47909db3ad2763bc43f2b1cdf95275ef9627c32b
Luc Drenth <luc.drenth@hotmail.com>
2023-07-28 15:11:22 +0200

    cleanup create-project script

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
+########################################################
+# We start with some constant and function definitions #
+########################################################
+
 COLOR_RED='\033[1;31m'
 COLOR_CYAN='\033[1;36m'
 COLOR_RESET='\033[0m'
+
+# $1 = color
+# $2 = message
+function log() {
+    printf "\n$1 $2 ${COLOR_RESET}\n\n"
+}
+
+function log_info() {
+    log "${COLOR_CYAN}" "$1"
+}
+function log_err() {
+    log "${COLOR_RED}" "$1"
+}
 
 NEW_PROJECT_NAME=$1
 LAZULI_DIRECTORY=$(realpath "$(dirname "$0")/..")
 TARGET_DIRECTORY=$(realpath "$(dirname "$LAZULI_DIRECTORY")")/$NEW_PROJECT_NAME
 
+
+
+#################################
+# The actual script starts here #
+#################################
+
+
 # Validate the given project name
 if [ -z "$NEW_PROJECT_NAME" ]; then
-    printf "\n${COLOR_RED}Please specify a project name ${COLOR_RESET}\n\n"
+    log_err "Please specify a project name"
     exit 1;
 fi
 
 if [ -d "$TARGET_DIRECTORY" ]; then
-    printf "\n${COLOR_RED}Project directory for '$NEW_PROJECT_NAME' already exists: $TARGET_DIRECTORY ${COLOR_RESET}\n\n"
+    log_err "Project directory for '$NEW_PROJECT_NAME' already exists: $TARGET_DIRECTORY"
     exit 1;
 fi
 
@@ -23,18 +47,18 @@ fi
 cd $LAZULI_DIRECTORY/..
 
 # Create our new project
-printf "\n${COLOR_CYAN}Creating new Lazuli project: $NEW_PROJECT_NAME ${COLOR_RESET}\n\n"
+log_info "Creating new Lazuli project: $NEW_PROJECT_NAME"
 cargo new $NEW_PROJECT_NAME
 
 # Step in to the newly created project
 cd ./$NEW_PROJECT_NAME
 
 # Add Lazuli as a dependency
-printf "\n${COLOR_CYAN}Adding Lazuli dependency ${COLOR_RESET}\n\n"
+log_info "Adding Lazuli as a dependency"
 cargo add lazuli --path ../lazuli
 
 # Copy over assets from Lazuli to our new project
-printf "\n${COLOR_CYAN}Copying over Lazuli assets ${COLOR_RESET}\n\n"
+log_info "Copying over Lazuli assets"
 
 printf "engine assets\n"
 cp -r ${LAZULI_DIRECTORY}/assets ./
@@ -55,7 +79,7 @@ printf "README.md\n"
 cp ${LAZULI_DIRECTORY}/project-template/README.md ./
 
 # Getting here means we successfully created the project
-printf "\n${COLOR_CYAN}Successfully created project '${NEW_PROJECT_NAME}' ${COLOR_RESET}\n\n"
+log_info "Successfully created project '${NEW_PROJECT_NAME}'"
 
 # If we are running on macOS, open the newly created project in the finder
 if [[ "$OSTYPE" == "darwin"* ]]; then
