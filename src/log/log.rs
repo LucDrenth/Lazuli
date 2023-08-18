ba0e9c35dea3d54a76a4d0a32e0d779efa946ebb
Luc Drenth <luc.drenth@hotmail.com>
2023-08-18 09:47:37 +0200

    use impl Into<String> instead of String for log msg parameter

--- src/log/log.rs
+++ src/log/log.rs
@@ -5,17 +5,18 @@ const COLOR_RESET: &str = "\x1B[0m";
 const SOURCE_ENGINE: &str = "ENGINE";
 const SOURCE_APP: &str = "APP";
 
-pub fn engine_info(msg: String) { log("", SOURCE_ENGINE, msg); }
-pub fn engine_warn(msg: String) { log(COLOR_YELLOW, SOURCE_ENGINE, msg); }
-pub fn engine_err(msg: String) { log(COLOR_RED, SOURCE_ENGINE, msg); }
+pub fn engine_info(msg: impl Into<String>) { log("", SOURCE_ENGINE, msg); }
+pub fn engine_warn(msg: impl Into<String>) { log(COLOR_YELLOW, SOURCE_ENGINE, msg); }
+pub fn engine_err(msg: impl Into<String>) { log(COLOR_RED, SOURCE_ENGINE, msg); }
 
-pub fn info(msg: String) { log("", SOURCE_APP, msg); }
-pub fn warn(msg: String) { log(COLOR_YELLOW, SOURCE_APP, msg); }
-pub fn err(msg: String) { log(COLOR_RED, SOURCE_APP, msg); }
+pub fn info(msg: impl Into<String>) { log("", SOURCE_APP, msg); }
+pub fn warn(msg: impl Into<String>) { log(COLOR_YELLOW, SOURCE_APP, msg); }
+pub fn err(msg: impl Into<String>) { log(COLOR_RED, SOURCE_APP, msg); }
 
-fn log(color: &str, source: &str, msg: String) {
+fn log(color: &str, source: &str, msg: impl Into<String>) {
+    let message = msg.into();
     let time = chrono::Local::now().format("%H:%M:%S");
-    let message_to_log = format!("{color}[{time}] {source}: {msg}{COLOR_RESET}");
+    let message_to_log = format!("{color}[{time}] {source}: {message}{COLOR_RESET}");
 
     println!("{message_to_log}");
 }
