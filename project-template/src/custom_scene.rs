5e187b6bd1246454ccec61d9e7fee37bec17ab59
Luc Drenth <luc.drenth@hotmail.com>
2023-09-04 21:44:04 +0200

    implement interface parameter in template its custom scene

--- project-template/src/custom_scene.rs
+++ project-template/src/custom_scene.rs
@@ -3,13 +3,13 @@ use lazuli::{graphics::scene::Scene, glam::Vec2, asset_manager::AssetManager, ev
 pub struct CustomScene {}
 
 impl Scene for CustomScene {
-    fn new(_event_system: &mut EventSystem, _window_size: Vec2, _pixel_density: f32, _asset_manager: &mut AssetManager) -> Result<Self, String> where Self: Sized {
+    fn new(_: &mut EventSystem, _: Vec2, _: f32, _: &mut AssetManager, _: &mut Interface) -> Result<Self, String> where Self: Sized {
         Ok(Self {  })
     }
 
-    unsafe fn draw(&self, _asset_manager: &mut AssetManager) {
+    unsafe fn draw(&self, _: &mut AssetManager) {
     }
 
-    fn update(&mut self, _event_system: &mut EventSystem, _input: &Input, _asset_manager: &mut AssetManager) {
+    fn update(&mut self, _: &mut EventSystem, _: &Input, _: &mut AssetManager, _: &mut Interface) {
     }
 }
