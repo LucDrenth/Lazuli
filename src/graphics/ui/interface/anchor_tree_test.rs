1d7038325c4ef86e4f871b0be811cbedddfbb873
Luc Drenth <luc.drenth@hotmail.com>
2024-04-30 21:20:20 +0200

    add more unit tests for AnchorTree

--- src/graphics/ui/interface/anchor_tree_test.rs
+++ src/graphics/ui/interface/anchor_tree_test.rs
@@ -78,16 +78,99 @@ fn test_get() -> Result<(), String> {
 }
 
 #[test]
+/// Test with the following tree:
+///
+///     1 --> 2 --> 3
+///            \
+///              --> 4 --> 5
+/// 
 fn test_get_children() -> Result<(), String> {
-    todo!()
+    let mut anchor_tree = AnchorTree::new();
+    let type_id = TypeId::of::<UiElementMock>();
+
+    let non_existing_id = ResourceId::new(0);
+    let parent_id = ResourceId::new(1);
+    let child = ResourceId::new(2);
+    let child_nested_1 = ResourceId::new(3);
+    let child_nested_2 = ResourceId::new(4);
+    let child_nested_2_nested = ResourceId::new(5);
+
+    anchor_tree.add_fixed_anchor(type_id, parent_id);
+    anchor_tree.add_element_anchor(type_id, &parent_id, type_id, child)?;
+    anchor_tree.add_element_anchor(type_id, &child, type_id, child_nested_1)?;
+    anchor_tree.add_element_anchor(type_id, &child, type_id, child_nested_2)?;
+    anchor_tree.add_element_anchor(type_id, &child_nested_2, type_id, child_nested_2_nested)?;
+
+    assert_eq!(0, anchor_tree.get_children(&non_existing_id).len());
+    assert_eq!(4, anchor_tree.get_children(&parent_id).len());
+    assert_eq!(3, anchor_tree.get_children(&child).len());
+    assert_eq!(1, anchor_tree.get_children(&child_nested_2).len());
+    assert_eq!(0, anchor_tree.get_children(&child_nested_2_nested).len());
+
+    Ok(())
 }
 
 #[test]
+/// Test with the following tree:
+///
+///     1 --> 2 --> 3
+///            \
+///              --> 4 --> 5
+/// 
 fn test_get_parent() -> Result<(), String> {
-    todo!()
+    let mut anchor_tree = AnchorTree::new();
+    let type_id = TypeId::of::<UiElementMock>();
+
+    let non_existing_id = ResourceId::new(0);
+    let parent_id = ResourceId::new(1);
+    let child = ResourceId::new(2);
+    let child_nested_1 = ResourceId::new(3);
+    let child_nested_2 = ResourceId::new(4);
+    let child_nested_2_nested = ResourceId::new(5);
+
+    anchor_tree.add_fixed_anchor(type_id, parent_id);
+    anchor_tree.add_element_anchor(type_id, &parent_id, type_id, child)?;
+    anchor_tree.add_element_anchor(type_id, &child, type_id, child_nested_1)?;
+    anchor_tree.add_element_anchor(type_id, &child, type_id, child_nested_2)?;
+    anchor_tree.add_element_anchor(type_id, &child_nested_2, type_id, child_nested_2_nested)?;
+
+    assert_eq!(true, anchor_tree.get_parent(&non_existing_id).is_none());
+    assert_eq!(4, *anchor_tree.get_parent(&child_nested_2_nested).unwrap().identifier().element_id.id());
+    assert_eq!(2, *anchor_tree.get_parent(&child_nested_2).unwrap().identifier().element_id.id());
+    assert_eq!(2, *anchor_tree.get_parent(&child_nested_1).unwrap().identifier().element_id.id());
+    assert_eq!(1, *anchor_tree.get_parent(&child).unwrap().identifier().element_id.id());
+    assert_eq!(true, anchor_tree.get_parent(&parent_id).is_none());
+
+    Ok(())
 }
 
 #[test]
+/// Test with the following tree:
+///
+///     1 --> 2 --> 3
+///            \
+///              --> 4 --> 5
+/// 
 fn test_remove_element_by_id() -> Result<(), String> {
-    todo!()
+    let mut anchor_tree = AnchorTree::new();
+    let type_id = TypeId::of::<UiElementMock>();
+
+    let non_existing_id = ResourceId::new(0);
+    let parent_id = ResourceId::new(1);
+    let child = ResourceId::new(2);
+    let child_nested_1 = ResourceId::new(3);
+    let child_nested_2 = ResourceId::new(4);
+    let child_nested_2_nested = ResourceId::new(5);
+
+    anchor_tree.add_fixed_anchor(type_id, parent_id);
+    anchor_tree.add_element_anchor(type_id, &parent_id, type_id, child)?;
+    anchor_tree.add_element_anchor(type_id, &child, type_id, child_nested_1)?;
+    anchor_tree.add_element_anchor(type_id, &child, type_id, child_nested_2)?;
+    anchor_tree.add_element_anchor(type_id, &child_nested_2, type_id, child_nested_2_nested)?;
+
+    assert_eq!(true, anchor_tree.remove_element_by_id(&non_existing_id).is_none());
+    assert_eq!(true, anchor_tree.remove_element_by_id(&child_nested_2).unwrap().identifier().element_id.equals(&child_nested_2));
+    assert_eq!(true, anchor_tree.remove_element_by_id(&child_nested_2).is_none());
+
+    Ok(())
 }
